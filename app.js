cb42751ccb080dca2cc66ff0519a1310c31b5f3e
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-05-08 05:40:57 -0700

    eCommerce

---
+++ app.js
@@ -0,0 +1,8 @@
+const btn1 = document.getElementById('btn-1')
+const btn2 = document.getElementById('btn-1')
+const cbContent = document.querySelector('.cb-content')
+
+btn2.onclick = () => {
+    cb-content.innerHTML
+}
+
