f5bff3ac78b834f9ccc2d54da9005b50b6d186ae
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-05-10 10:08:42 -0700

    eCommerce (API-data)

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const btn1 = document.getElementById('btn-1')
-const btn2 = document.getElementById('btn-1')
-const cbContent = document.querySelector('.cb-content')
-
-btn2.onclick = () => {
-    cb-content.innerHTML
+const getData = e => {
+    fetch('http://localhost:3000/products')
+        .then(res => res.json())
+        .then(data => {
+            getProducts(data);
+        })
+        .catch(err => console.log(err))
 }
 
+getData()
+
+
+const getProducts = data => {
+    data.forEach(products => {
+        let data = `
+        <div class="discover-cards">
+                <div class="card">
+                    <img src="../imgs/unsplash_KjRkxQ2NNXA.png" alt="girl">
+                    <div class="card-content">
+                        <h4>Plain White Shirt</h4>
+                        <span>$29.00</span>
+                    </div>
+                </div>
+        </div>
+        `
+    });
+}
