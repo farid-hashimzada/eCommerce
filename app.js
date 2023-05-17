1d21e980405f7415df6925f72e2035ce6713775c
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-05-17 07:30:19 -0700

    eCommerce(add to card)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const url = 'http://localhost:3000/products/'
 const getData = e => {
-    fetch('http://localhost:3000/products')
+    fetch(url)
         .then(res => res.json())
         .then(data => {
             getProducts(data);
@@ -17,7 +18,7 @@ const getProducts = data => {
                 <div class="card">
                     <img src=${products.image}>
                     <div class="card-content">
-                        <a href="#" class="viewProduct" data-key=${products.id}>${products.description}</a>
+                        <a href="javascript:void(0)" class="viewProduct" data-key=${products.id}>${products.description}</a>
                         <span>${products.price} ${products.currency}</span>
                     </div>
                 </div>
@@ -37,21 +38,42 @@ const getModal = (data, currentProductId) => {
     const productImage = document.querySelector('#productImage')
     const cardPrice = document.querySelector('.card-price')
     const productDescription = document.querySelector('#productDescription')
-
+    const addToCartButton = document.querySelector('#addToCartButton')
 
 
     data.forEach((product) => {
         if (product.id === currentProductId) {
-            console.log(product)
             productImage.src = product.image
             cardPrice.innerHTML = product.price + " " + product.currency
             productDescription.innerHTML = product.description
 
             ProductModal.style.display = 'flex'
-            document.querySelector('body').style.overflow = "hidden"
-        }
-    })
-}
+            document.querySelector('body').style.overflow = "hidden";
+
+            addToCartButton.addEventListener("click", (e) => {
+                fetch(url + product.id, {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        addedBasket: true,
+                    }),
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        e.target.innerHTML = 'Product add to card' // Handle the response from the server
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                    });
+            });
+
+        };
+
+    });
+
+};
 
 closeModal.addEventListener('click', e => {
     document.querySelector('body').style.overflow = "visible"
